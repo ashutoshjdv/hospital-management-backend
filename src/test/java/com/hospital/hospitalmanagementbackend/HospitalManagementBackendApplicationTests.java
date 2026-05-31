d5c8def7ea1907d0a6c5608b56de3fe697e5c42e
ashutosh jadhav <jdvoct02@gmail.com>
2026-05-31 11:35:05 +0530

    initial setup for hospital management project.

---
+++ src/test/java/com/hospital/hospitalmanagementbackend/HospitalManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.hospitalmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
